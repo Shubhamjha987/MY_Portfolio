4786f96f32d2dc439d6264bb04a7140bfc4f810b
Shubhamjha987 <20051757@kiit.ac.in>
2025-06-11 14:04:02 +0530

    prepare for GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from "path"
-// https://vite.dev/config/
+import path from 'path'
+
+// Set the repo name below
+const repoName = 'MY_Portfolio' // 🚨 Must match your GitHub repo name
+
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: `/${repoName}/`, // 👈 REQUIRED for GitHub Pages
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname,"./src"),
-    }
-  }
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
